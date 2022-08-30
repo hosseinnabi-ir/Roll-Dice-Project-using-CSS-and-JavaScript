dfd0035bc1a4cb3158ae7ffb41c09333a75f87e9
CODE WITH HOSSEIN <68454951+hosseinnabi-ir@users.noreply.github.com>
2022-08-30 16:08:57 +0430

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const dice = document.querySelector('.dice');
+const rollBtn = document.querySelector('.roll');
+
+const randomDice = () => {
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random >= 1 && random <= 6) {
+        rollDice(random);
+    }
+    else {
+        randomDice();
+    }
+}
+
+const rollDice = random => {
+
+    dice.style.animation = 'rolling 4s';
+
+    setTimeout(() => {
+
+        switch (random) {
+            case 1:
+                dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                break;
+
+            case 6:
+                dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                break;
+
+            case 2:
+                dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                break;
+
+            case 5:
+                dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                break;
+
+            case 3:
+                dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                break;
+
+            case 4:
+                dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                break;
+
+            default:
+                break;
+        }
+
+        dice.style.animation = 'none';
+
+    }, 4050);
+
+}
+
+rollBtn.addEventListener('click', randomDice);
